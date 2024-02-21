7ad41f94f0c674047e2e0eabf01e3376cd491b66
adm-nkowenski <adm-nkowenski@ethz.ch>
2024-02-21 13:34:23 +0100

    Update to new wazuh-control binary§

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the agent
-/var/ossec/bin/ossec-control start
+/var/ossec/bin/wazuh-control start
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start agent: $status"
@@ -11,7 +11,7 @@ fi
 echo "background jobs running, listening for changes"
 
 while sleep 60; do
-  /var/ossec/bin/ossec-control status > /dev/null 2>&1
+  /var/ossec/bin/wazuh-control status > /dev/null 2>&1
   status=$?
   if [ $status -ne 0 ]; then
     echo "looks like the agent died."
